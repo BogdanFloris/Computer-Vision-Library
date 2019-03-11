12188374388d1de3504d54763c94e0ab8cddc866
Bogdan Floris <bogdan.floris@gmail.com>
2019-03-12 00:18:11 +0100

    implemented bilinear interpolation

--- demo.py
+++ demo.py
@@ -1,5 +1,11 @@
-# resize with nearest neighbour interpolation
 from uwimg import *
+
+# resize with nearest neighbour interpolation
 im = load_image("data/dogsmall.jpg")
 a = nn_resize(im, im.w*4, im.h*4)
 save_image(a, "dog4x-nn")
+
+# resize with bilinear interpolation
+im = load_image("data/dogsmall.jpg")
+a = bilinear_resize(im, im.w*4, im.h*4)
+save_image(a, "dog4x-bl")
--- src/resize_image.c
+++ src/resize_image.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <math.h>
 #include "image.h"
 
@@ -31,13 +32,46 @@ image nn_resize(image im, int w, int h)
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
-    // TODO
-    return 0;
+    // floor x and y
+    int xi = (int) floorf(x);
+    int yi = (int) floorf(y);
+    // get the values for all four corners
+    float v1 = get_pixel(im, xi, yi, c);
+    float v2 = get_pixel(im, xi + 1, yi, c);
+    float v3 = get_pixel(im, xi, yi + 1, c);
+    float v4 = get_pixel(im, xi + 1, yi + 1, c);
+    // get distances
+    float d1 = x - xi;
+    float d2 = 1 - d1;
+    float d3 = y - yi;
+    float d4 = 1 - d3;
+    // calculate q values
+    float q1 = v1 * d2 + v2 * d1;
+    float q2 = v3 * d2 + v4 * d1;
+    float q = q1 * d4 + q2 * d3;
+    return q;
 }
 
 image bilinear_resize(image im, int w, int h)
 {
-    // TODO
-    return make_image(1,1,1);
+    image resize_im = make_image(w, h, im.c);
+    float a_x = im.w / (float) w;
+    float b_x = (float) (-0.5 + 0.5 * a_x);
+    float a_y = im.h / (float) h;
+    float b_y = (float) (-0.5 + 0.5 * a_y);
+    int x, y, c;
+    for (y = 0; y < h; ++y) {
+        for (x = 0; x < w; ++x) {
+            // map to old coordinate system
+            float old_x = a_x * x + b_x;
+            float old_y = a_y * y + b_y;
+            for (c = 0; c < im.c; ++c) {
+                // nearest neighbour interpolation
+                float val = bilinear_interpolate(im, old_x, old_y, c);
+                set_pixel(resize_im, x, y, c, val);
+            }
+        }
+    }
+    return resize_im;
 }
 
