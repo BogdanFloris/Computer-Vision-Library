24708a6ee4b537480f37ff5d12eda4448033730b
Bogdan Floris <bogdan.floris@gmail.com>
2019-03-12 18:33:39 +0100

    demo for hybrid images

--- demo.py
+++ demo.py
@@ -28,3 +28,13 @@ im = load_image("data/dog.jpg")
 f = make_gaussian_filter(2)
 blur = convolve_image(im, f, 1)
 save_image(blur, "dog-gauss2")
+
+# hybrid images
+im = load_image("data/dog.jpg")
+f = make_gaussian_filter(2)
+lfreq = convolve_image(im, f, 1)
+hfreq = im - lfreq
+reconstruct = lfreq + hfreq
+save_image(lfreq, "low-frequency")
+save_image(hfreq, "high-frequency")
+save_image(reconstruct, "reconstruct")
